a0c91274eb567d3b5fbc010bcdc269d3fd0af30e
rrios19 <ronaldrios19@icloud.com>
2022-10-16 11:46:08 -0600

    Interface modification

--- interface.sv
+++ interface.sv
@@ -1,21 +1,21 @@
 // Tecnologico de Costa Rica
 // Verificacion funcional de circuitos integrados
 // S2 2022
-// Proyecto 2: Floating point multipliers
+// Project 2: Floating point multipliers
 // Irene Prieto 
 // Ronald Rios
 
-// Interface que conecta el dUT
+// Interface to connect the DUT
 interface dut_if(input bit clk);
-	logic [2:0]r_mode;               // Redondeo
-	logic [31:0]fp_X, fp_Y, fp_Z;	 // Entradas X,Y. Resultado Z
-	logic ovrf,undrf;		 // Overflow and onderflow flags
+	logic [2:0]r_mode;               // Rounding
+	logic [31:0]fp_X, fp_Y, fp_Z;	 // Inputs X,Y. Result Z
+	logic ovrf,udrf;		 // Overflow and underflow flags
 
 	clocking cb @(posedge clk);
 		default input #1step output #3ns;
-		input ovrf
-		input undrf;
-		input fp_Z;
+		input  ovrf
+		input  udrf;
+		input  fp_Z;
 		output fp_X;
 		output fp_Y;
 		output r_mode;
