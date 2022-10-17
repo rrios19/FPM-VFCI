2c523e1d9b971b25bd265d3c4e04540fc0d4e482
rrios19 <ronaldrios19@icloud.com>
2022-10-16 23:19:35 -0600

    Driver modification

--- driver.sv
+++ driver.sv
@@ -23,18 +23,18 @@ class driver extends uvm_driver #(item);
 	virtual task run_phase(uvm_phase phase);
 		super.run_phase(phase);
 		forever begin
-			item myitem; // New pointer myitem of type item
-			vif.cb.fp_X <= myitem.fp_X; // Puts the value "X" of the item in the out "X" of the interface
+			item itm; // New pointer myitem of type item
 			`uvm_info("Driver",$sformatf("Wait for item from sequence"),UVM_HIGH)
-			seq_item_port.get_next_item(myitem); // It gets the item from the sequence port
-			driver_item(mytrans); 		     // It calls the task to process the item
-			seq_item_port.item_done(); 	     // It allows the port to get a new item when the item is done
+			seq_item_port.get_next_item(itm); // It gets the item from the sequence port
+			driver_item(itm); 	          // It calls the task to process the item
+			seq_item_port.item_done();        // It allows the port to get a new item when the item is done
 	endtask
 
-	virtual task driver_item(item myitem);
-		@(vif.cb); // On a positive edge of the "cb"  clock
-			vif.cb.fp_X   <= myitem.fp_X; // Puts the value "X" of the item, in the out "X" of the interface
-			vif.cb.fp_Y   <= myitem.fp_Y; // Puts the value "Y" of the item, in the out "Y" of the interface
-			vif.cb.r_mode <= myitem.fp_X; // Puts the value "r" of the item, in the out "r" of the interface
+	virtual task driver_item(item itm);
+		@(vif.cb);begin // On a positive edge of the "cb"  clock
+			vif.cb.fp_X   <= itm.fp_X; // Puts the value "X" of the item, in the out "X" of the interface
+			vif.cb.fp_Y   <= itm.fp_Y; // Puts the value "Y" of the item, in the out "Y" of the interface
+			vif.cb.r_mode <= itm.fp_X; // Puts the value "r" of the item, in the out "r" of the interface
+		end
 	endtask
 endclass
