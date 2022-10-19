77dd6cdd40e2bc32a6799017cbaf64ff77dd9679
rrios19 <ronaldrios19@icloud.com>
2022-10-18 23:09:42 -0600

    Agent modification

--- agent.sv
+++ agent.sv
@@ -12,9 +12,19 @@ class agent extends uvm_agent;
 		super.new(name,parent);
 	endfunction
 
-	driver 			drv_inst;
-	monitor 	        mnt_inst;
-	uvm_sequencer #(item) 	seq_inst;
+	driver 			drv_inst;	// Driver instance
+	monitor 	        mnt_inst;	// Monitor instance
+	uvm_sequencer #(item) 	seq_inst;	// Sequencer instance
 	
 	virtual function void build_phase(uvm_phase phase)
+		super.build_phase(phase);
+		seq_inst = uvm_sequencer#(item)::type_id::create("seq_inst",this);
+		drv_inst = driver::type_id::create("drv_inst",this);
+		mnt_inst = monitor::type_id::create("mnt_inst",this);
+	endfunction
+	
+	virtual function connect_phase(uvm_phase phase);
+		super.connect_phase(phase);
+		drv_inst.seq_item_port.connect(seq_inst.seq_item_export);
+	endfunction
 endclass
