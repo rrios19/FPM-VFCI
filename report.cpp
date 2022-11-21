c91ae0ca375312e9372b9773367e500219849726
rrios19 <ronaldrios19@icloud.com>
2022-11-21 02:08:53 -0600

    C++ function

--- report.cpp
+++ report.cpp
@@ -7,9 +7,8 @@ using namespace std;
 extern "C" int report()
 {
 	FILE * rp;
-	rp = fopen("prueba.csv","a");
-	//fprintf(rp,"%s","X,Y,Rounding mode,Overflow,Underflow,Z,Expected Z");
-	fprintf(rp,"%s","siii");
+	rp = fopen("report.csv","a");
+	fprintf(rp,"%s","X,Y,Rounding mode,Overflow,Underflow,Z,Expected Z");
 	fclose(rp);
 	return 0;
 }
